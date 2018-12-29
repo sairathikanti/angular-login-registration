cfbec762b8db2a7d975c2e486667c66fd8b28ba8
saikrishna <vamshi.sai.9866@gmail.com>
2018-12-29 18:27:11 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RouterModule, Routes} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {HttpModule} from "@angular/http";
+import { MatInputModule} from '@angular/material';
+import { AppComponent } from './app.component';
+import {MatTableModule} from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+const appRoutes: Routes = [
+   {
+      path: '',
+      component: UserloginComponent
+   },
+   {
+      path: 'app-mainpage',
+      component: MainpageComponent
+   }
+];
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      UserloginComponent,
+      MainpageComponent
+   ],
+   
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot(appRoutes),
+      BrowserAnimationsModule,
+      HttpModule,
+      MatInputModule,
+      MatTableModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
+})
+export class AppModule { }
